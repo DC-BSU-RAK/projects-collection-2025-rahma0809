0213ab1c4aff11f0ced34ad796b1ee5e9a898055
Rahma Reyaz <rahmareyaz91@gmail.com>
2025-05-22 10:24:07 +0400

    Added both projects

---
+++ Novel Nudge/app/src/main/java/com/example/novelnudge/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.example.novelnudge
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Wait for 5 seconds, then go to MainActivity
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Finish SplashActivity so user can’t return to it
+        }, 3000)
+
+
+    }
+}
